32b3a6efb265cb05bdaeb58725d54f986b20f547
Enrique <mt16007@ues.edu.sv>
2018-11-01 23:40:02 -0600

    Creacion de la base del proyecto SIHPSA y algunos modelos

--- sihpsa/sihpsa/settings.py
+++ sihpsa/sihpsa/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sihpsa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,4 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL='/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_env", "static_root")
+MEDIA_DIRS = (os.path.join(BASE_DIR, 'static/media'),)
