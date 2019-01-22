1feac862a8ad12aa732bc9fa4d578b898cd3cd5a
AP16014 <ap16014@ues.edu.sv>
2019-01-21 22:58:24 -0600

    test

--- sihpsa/sihpsa/settings.py
+++ sihpsa/sihpsa/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'slk!6lw+wm%n#(j35t+ydgd*iztq9^+sd+f+f&_!j4a_8jj(x_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -136,4 +136,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_env", "static_root")
 MEDIA_DIRS = (os.path.join(BASE_DIR, 'static/media'),)
-ALLOWED_HOSTS = ['142.93.202.162']
+#ALLOWED_HOSTS = ['142.93.202.162']
