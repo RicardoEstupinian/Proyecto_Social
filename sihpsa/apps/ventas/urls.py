884c91b8ca35e6cb456950ddb252df17df19f349
peluca <sebas2800_ff@outlook.com>
2019-01-21 23:17:08 -0600

    se agrego la lista de ventas y se agrego la validacion del atributo vendible en la view (experimental)

--- sihpsa/apps/ventas/urls.py
+++ sihpsa/apps/ventas/urls.py
@@ -1,10 +1,12 @@
 
 from apps.ventas import views
 from django.urls import path 
-from apps.ventas.views import Cargar
+from apps.ventas.views import Cargar, Lista
+from django.conf.urls import url, include
 
 app_name = "ventas"
 
 urlpatterns = [
     path('', views.Cargar,name="venta"),
+    path('Lista/', views.Lista.as_view(), name="Lista"),
 ]
